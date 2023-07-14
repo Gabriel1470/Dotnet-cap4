cf373b7bb96154b01c12d105569a5fa1e806f5a8
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-14 14:21:43 +0200

    dda finire

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dotnet_cap4;
+var myQueueInt= new MyQueue<int>();
+
+myQueueInt.Queue(1);
+myQueueInt.Queue(2);
+myQueueInt.Queue(3);
+myQueueInt.Queue(4);
+
+myQueueInt.Dequeue();
+myQueueInt.Peek();
+
+
+
+var myQueueStr = new MyQueue<string>();
+
+myQueueStr.Queue("Red");
+myQueueStr.Queue("Gio");
+myQueueStr.Queue("Manu");
+myQueueStr.Queue("Albe");
+
+myQueueString.Dequeue();
+myQueueString.Peek();
